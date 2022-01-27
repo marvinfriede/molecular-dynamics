6ee0f33d10c5e5563fd93a5f2fbccbb5de621a0f
marvinfriede <51965259+marvinfriede@users.noreply.github.com>
2022-01-27 11:45:32 +0100

    finished program with LJ potential and PBC

--- app/main.f90
+++ app/main.f90
@@ -1,14 +1,16 @@
 program main
-  !> prints a matrix quantity to screen
+  !> prints a matrix quantity to screen for debug
   !  examples:
   !  call write_vector(vec, name='vector')
   !  call write_matrix(mat, name='matrix')
   !  call write_matrix(mat, name='packed matrix')
-  use print_matrix, only: write_matrix
+  !use print_matrix, only: write_matrix
 
   use build_box
   use algorithm
   implicit none
+
+  intrinsic :: sqrt
 
   !> floating point accuracy
   integer, parameter :: wp = selected_real_kind(15)
@@ -31,9 +33,6 @@ program main
   !> whatever
   real(wp), dimension(3, natom) :: a
 
-  !> force constant
-  real(wp), parameter :: k = 1.0_wp
-
   !> number of iterations
   integer, parameter :: itime = 200
 
@@ -43,11 +42,13 @@ program main
   !> mass of particle
   real(wp), parameter :: mass = 39.948_wp
 
+  !> temperature
+  real(wp) :: T
   !> temperature to rescale velocities
-  integer, parameter :: t_rescale = 8
+  integer, parameter :: T_req = 8
 
   !> potential energy
-  real(wp) :: e_pot_harm
+  real(wp) :: e_pot
   !> kinetic energy
   real(wp) :: e_kin
   !> total energy
@@ -56,43 +57,50 @@ program main
   !> iteration variables
   integer :: i, j, w
 
-  a = 0.0_wp
+  !> temporary variable
+  real(wp) :: temp
+
+  ! initial temperature scaling (1 means no scaling)
+  temp = 1.0_wp
+
+  ! initial velocities
   v = 0.0_wp
 
-  !> write energies
+  ! write energies
   open (unit=15, file="out/energy.csv")
-  write (15, "(A27)") "time,e_kin,e_pot_harm,e_tot"
+  write (15, "(A22)") "time,e_kin,e_pot,e_tot"
 
-  !> pass 1 for sc and 4 for fcc
+  ! pass 1 for sc and 4 for fcc
   call build_grid(xyz, natom, l, 4)
 
-  !> calc for initial
-  call calc_e_kin(natom, v, mass, e_kin)
-  call calc_pot_harm(natom, xyz, a, l, k, e_pot_harm)
-  e_tot = e_kin + e_pot_harm
+  ! evaluate forces and hence f/m from positions
+  call calc_force(natom, xyz, f, l, e_pot)
 
-  !> write trajectory for starting positions
+  ! write trajectory for starting positions
   open (unit=16, file="out/trj.xyz")
   write (16, "(I3)") natom
-  write (16, "(F15.10)") e_tot
+  write (16, "(F15.8)") e_pot ! e_kin = 0 in first step
   do w = 1, natom
     write (16, *) "Ar", xyz(1, w), xyz(2, w), xyz(3, w)
   end do
 
-  !> evaluate forces and hence f/m from positions
-  call calc_force(natom, xyz, f, a, l, k)
-
   time_prop: do i = 1, itime
+    ! scale factor for velocities
+    if (i .gt. 1) then
+      T = e_kin/3.0_wp/natom
+      temp = sqrt(T_req/T)
+    end if
+
     do j = 1, natom
-      ! propagate velocities (half step)
-      v(:, j) = v(:, j) + 0.5_wp*f(:, j)/mass*delta
+      ! scale and propagate velocities (half step)
+      v(:, j) = temp*(v(:, j) + 0.5_wp*f(:, j)/mass*delta)
 
       ! propagate positions
       xyz(:, j) = xyz(:, j) + v(:, j)*delta
     end do
 
-    !> evaluate forces and hence f/m from positions
-    call calc_force(natom, xyz, f, a, l, k)
+    ! evaluate forces and hence f/m from positions
+    call calc_force(natom, xyz, f, l, e_pot)
 
     do j = 1, natom
       ! propagate velocities again (half step)
@@ -100,20 +108,19 @@ program main
     end do
 
     call calc_e_kin(natom, v, mass, e_kin)
-    call calc_pot_harm(natom, xyz, a, l, k, e_pot_harm)
-    e_tot = e_kin + e_pot_harm
+    e_tot = e_kin + e_pot
 
     ! write energies to console
     write (*, "(A16,2X,F15.8)") "Kinetic Energy =", e_kin
-    write (*, "(A18,F15.8)") "Potential Energy =", e_pot_harm
+    write (*, "(A18,F15.8)") "Potential Energy =", e_pot
     write (*, "(A14,4X,F15.8)") "Total Energy =", e_tot
 
     ! write energies to file
-    write (15, "(F15.10,A1,F15.10,A1,F15.10,A1,F15.10)") delta*i, ",", e_kin, ",", e_pot_harm, ",", e_tot
+    write (15, "(F15.8,A1,F15.8,A1,F15.8,A1,F15.8)") delta*i, ",", e_kin, ",", e_pot, ",", e_tot
 
     ! write trajectory
     write (16, "(I3)") natom
-    write (16, "(F15.10)") e_tot
+    write (16, "(F15.8)") e_tot
     do w = 1, natom
       write (16, *) "Ar", xyz(1, w), xyz(2, w), xyz(3, w)
     end do
--- src/algorithm.f90
+++ src/algorithm.f90
@@ -5,69 +5,78 @@ module algorithm
   !> floating point accuracy
   integer, parameter :: wp = selected_real_kind(15)
 
-  public :: calc_force, calc_pot_harm, calc_e_kin
+  public :: calc_e_kin, calc_force
 contains
-  subroutine calc_force(natom, xyz, f, a, l, k)
+  subroutine calc_force(natom, xyz, f, l, e_pot_lj)
     implicit none
 
     !> number of atoms
     integer, intent(in) :: natom
 
-    !> atom coordinates of the system
+    !> atom coordinates of particles
     real(wp), dimension(3, natom), intent(in) :: xyz
+    !> distance vector between two particles
+    real(wp), dimension(3) :: xyz_r
+    !> squared distance between two particles
+    real(wp) :: xyzij2
 
     !> forces
     real(wp), dimension(3, natom), intent(out) :: f
-
-    !> whatever
-    real(wp), dimension(3, natom), intent(in) :: a
-
-    !> length of cubic box
-    real(wp), intent(in) :: l
-
-    !> force constant
-    real(wp), intent(in) :: k
-
-    !> iteration
-    integer :: i
-
-    f = 0.0_wp
-    do i = 1, natom
-      f(:, i) = -k*(xyz(:, i) - a(:, i))
-    end do
-
-  end subroutine calc_force
-
-  subroutine calc_pot_harm(natom, xyz, a, l, k, e_pot_harm)
-    implicit none
-
-    !> number of atoms
-    integer, intent(in) :: natom
-
-    !> atom coordinates of the system
-    real(wp), dimension(3, natom), intent(in) :: xyz
-
-    !> whatever
-    real(wp), dimension(3, natom), intent(in) :: a
+    !> force: derivate of LJ potential
+    real(wp) :: ff
 
     !> length of cubic box
     real(wp), intent(in) :: l
 
-    !> force constant
-    real(wp), intent(in) :: k
+    !> parameter in LJ potential
+    real(wp), parameter :: sigma = 3.405_wp
+    real(wp), parameter :: sigma2 = sigma*sigma
+    real(wp) :: sigmar2
+    real(wp) :: sigmar6
+    real(wp) :: sigmar12
+
+    !> parameter in LJ potential
+    real(wp), parameter :: epsilon = 120.0_wp
+
+    !> cutoff parameters for interaction radius
+    real(wp) :: rcutoff
+    real(wp) :: rcutoff2
 
     !> potential energy
-    real(wp), intent(out) :: e_pot_harm
+    real(wp), intent(out) :: e_pot_lj
 
     !> iteration variable
-    integer :: i
+    integer :: i, j
 
-    e_pot_harm = 0.0_wp
-    do i = 1, natom
-      e_pot_harm = e_pot_harm + 0.5_wp*k*sum((xyz(:, i) - a(:, i))**2)
+    rcutoff = 0.50_wp*l
+    rcutoff2 = rcutoff*rcutoff
+
+    f = 0.0_wp
+    e_pot_lj = 0.0_wp
+    do i = 1, natom - 1
+      do j = i + 1, natom
+        ! periodic boundary conditions
+        xyz_r = xyz(:, i) - xyz(:, j)
+        xyz_r = xyz_r - l*anint(xyz_r/l)
+
+        xyzij2 = sum(xyz_r**2)
+        if (xyzij2 .lt. rcutoff2) then
+          sigmar2 = sigma2/xyzij2
+          sigmar6 = sigmar2*sigmar2*sigmar2
+          sigmar12 = sigmar6*sigmar6
+
+          ! update force
+          ff = (48.0_wp*epsilon*(sigmar12 - (0.5_wp*sigmar6)))/xyzij2
+          f(:, i) = f(:, i) + ff*xyz_r
+          f(:, j) = f(:, j) + ff*xyz_r
+
+          ! energy evaluation
+          e_pot_lj = e_pot_lj + sigmar12 - sigmar6 + 3.83738839608178386E-3_wp
+        end if
+      end do
     end do
-
-  end subroutine calc_pot_harm
+    e_pot_lj = 4.0_wp*epsilon*e_pot_lj
+  end subroutine calc_force
 
   subroutine calc_e_kin(natom, v, mass, e_kin)
     implicit none
@@ -93,4 +102,65 @@ contains
     end do
 
   end subroutine calc_e_kin
+
+  !*********************************************************
+  !****************** HARMONIC OSCILLATOR ******************
+  !*********************************************************
+
+  subroutine calc_force_harm(natom, xyz, f, a)
+    implicit none
+
+    !> number of atoms
+    integer, intent(in) :: natom
+
+    !> atom coordinates of the system
+    real(wp), dimension(3, natom), intent(in) :: xyz
+
+    !> forces
+    real(wp), dimension(3, natom), intent(out) :: f
+
+    !> whatever
+    real(wp), dimension(3, natom), intent(in) :: a
+
+    !> force constant
+    real(wp), parameter :: k = 1.0_wp
+
+    !> iteration
+    integer :: i
+
+    f = 0.0_wp
+    do i = 1, natom
+      f(:, i) = -k*(xyz(:, i) - a(:, i))
+    end do
+
+  end subroutine calc_force_harm
+
+  subroutine calc_pot_harm(natom, xyz, a, k, e_pot_harm)
+    implicit none
+
+    !> number of atoms
+    integer, intent(in) :: natom
+
+    !> atom coordinates of the system
+    real(wp), dimension(3, natom), intent(in) :: xyz
+
+    !> whatever
+    real(wp), dimension(3, natom), intent(in) :: a
+
+    !> force constant
+    real(wp), intent(in) :: k
+
+    !> potential energy
+    real(wp), intent(out) :: e_pot_harm
+
+    !> iteration variable
+    integer :: i
+
+    e_pot_harm = 0.0_wp
+    do i = 1, natom
+      e_pot_harm = e_pot_harm + 0.5_wp*k*sum((xyz(:, i) - a(:, i))**2)
+    end do
+
+  end subroutine calc_pot_harm
+
 end module algorithm
